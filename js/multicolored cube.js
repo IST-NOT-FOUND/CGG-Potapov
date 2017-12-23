2e010c4f23a38da3e7137a3fdd05563442d129fb
shorox_7 <shorox102@gmail.com>
2017-12-23 21:35:45 +0300

    Глава №5, Задание №12. Разноцветный куб

---
+++ js/multicolored cube.js
@@ -0,0 +1,30 @@
+var gl;
+var shaderProgram;
+var vertexBuffer; // буфер вершин
+var indexBuffer; //буфер индексов
+var colorBuffer; //буфер цветов
+
+var mvMatrix = mat4.create();
+var pMatrix = mat4.create();
+// установка шейдеров
+function initShaders() {
+    var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
+    var vertexShader = getShader(gl.VERTEX_SHADER, 'shader-vs');
+
+    shaderProgram = gl.createProgram();
+
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Не удалось установить шейдеры");
+    }
+
+    gl.useProgram(shaderProgram);
+
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
